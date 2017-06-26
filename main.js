213aa594916451d62248688b0733b865bb8e14a8
doccindeme <doccindeme@gmail.com>
2017-06-26 18:27:25 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         print-your-report
+// @namespace    http://github.com/doccindeme/
+// @version      0.1
+// @description  Allows you to print or save in a pdf your report from Nightscout
+// @author       doccindeme
+// @match        *.herokuapp.com
+// @match        *.azurewebsites.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("hello");
+    // Your code here...
+})();
